9bfb9fc932d84247bd3044b2228ef68a867535c4
thutuva <thuxuan@tuvahealth.com>
2023-08-24 12:20:44 +0000

    removed ref to delete model

--- models/core/elation_core__observation.sql
+++ models/core/elation_core__observation.sql
@@ -1,6 +1,6 @@
 {{ dbt_utils.union_relations(
 
-    relations=[ref('elation_stage__health_maintenance'), ref('elation_stage__report'), ref('elation_stage__misc_orders')]
+    relations=[ref('elation_stage__health_maintenance'), ref('elation_stage__report')]
                 , source_column_name = none
 
 ) }}
