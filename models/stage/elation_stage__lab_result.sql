0684b5b2583b73ff73ad9d87b8755ef4005655e3
thutuva <101137626+thutuva@users.noreply.github.com>
2024-06-06 16:34:23 -0400

    fixing data type

--- models/stage/elation_stage__lab_result.sql
+++ models/stage/elation_stage__lab_result.sql
@@ -32,7 +32,7 @@ select
     , result.reference_max as source_reference_range_high
     , cast(null as {{ dbt.type_string() }} ) as normalized_reference_range_low
     , cast(null as {{ dbt.type_string() }} ) as normalized_reference_range_high
-    , result.is_abnormal as source_abnormal_flag
+    , cast(result.is_abnormal as {{ dbt.type_string() }} ) as source_abnormal_flag
     , cast(null as {{ dbt.type_string() }} ) as normalized_abnormal_flag
     , ord.ordering_provider as ordering_practitioner_id
     , cast(null as {{ dbt.type_string() }} ) as specimen
